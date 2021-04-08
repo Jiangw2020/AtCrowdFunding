7d7480c5c82b45ee58809f14fdd0dc3fa074c9aa
jiangwei <734145091@qq.com>
2021-04-08 16:49:26 +0800

    init

--- atcrowdfunding05-common-util/src/main/java/jw/crowd/Oss.java
+++ atcrowdfunding05-common-util/src/main/java/jw/crowd/Oss.java
@@ -9,16 +9,16 @@ import static jw.crowd.util.CrowdUtil.uploadFileToOss;
 
 public class Oss {
     public static void main(String[] args) throws FileNotFoundException {
-        FileInputStream fileInputStream = new FileInputStream("*************");
+        FileInputStream fileInputStream = new FileInputStream("C:\\Users\\ASUS\\Pictures\\Camera Roll\\1.jpg");
         System.out.println();
         ResultEntity<String> resultEntity = uploadFileToOss(
-                "*************",
-                "*************",
-                "*************",
+                "http://oss.aliyuncs.com",
+                "LTAI4GKTr7bJ",
+                "7bqohh0RvHn3EsFsL6",
                  fileInputStream,
-                "*************",
-                "*************",
-                "*************");
+                "jw2021",
+                "http://jw2021.oss-cn-hangzhou.aliyuncs.com",
+                "1.jpg");
         System.out.println(resultEntity);
     }
 }
